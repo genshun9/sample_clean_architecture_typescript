1a510a52c4bb8c9989e1fa20857df3e2e371110b
genshun <genshun9@gmail.com>
2025-02-24 16:36:53 +0900

    ControllerとUseCaseを1:1にして共通メソッドにする案を廃止

--- src/shared/adaptor/Controller.ts
+++ src/shared/adaptor/Controller.ts
@@ -10,35 +10,21 @@ export abstract class Controller<RequestDto> {
 
     protected abstract validateRequest(req: RequestDto): boolean;
 
-    // controllerとusecaseが1対1であるなら基底クラスに共通実装可能なはずだが、やっぱ無理だったのでやめる
-    run(res: Response, req: RequestDto): void {
-        const validationResult = this.validateRequest(req);
-        switch(validationResult) {
-            case true:
-                this.useCase.execute(req);
-                break;
-            case false:
-                this.presenter.failure(new Error("validation error"));
-            default:
-                break;
-        }
-    }
-
-    // protected abstract validateRequest(req: Request): boolean;
-    // protected abstract handleSuccess(result: Success<Response>): any;
-    // protected abstract handleFailure(error: Error): any; // SuccessとFailure型を返したほうがキレイな気がする
-
-    // async execute(req: Request): void {
-    //     try {
-    //         const request = this.validateRequest(req);
-    //         const result = await this.useCase.execute(request);
-    //         if (UseCaseResult.isSuccess(result)) {
-    //             return this.handleSuccess(result);
-    //         } else {
-    //             return this.handleFailure(result.error);
-    //         }
-    //     } catch (error) {
-    //         return this.handleFailure(error);
+    // controllerとusecaseが1対1なら、基底クラスに共通処理として実装できそう。
+    // 実際はそんなことはないのでやめた、雰囲気コードだけ残す。
+    // run(res: Response, req: Request): void {
+    //     // request自体のバリデーション
+    //     const validationResult = this.validateRequest(req);
+    //     switch(validationResult) {
+    //         case true:
+    //             // usecaseの実装し内部でpresenterを実行しresponse返す
+    //             this.useCase.execute(req);
+    //             break;
+    //         case false:
+    //             // usecase行く前にpresenterのfailureを実行するが、微妙かも
+    //             this.presenter.failure(new Error("validation error"));
+    //         default:
+    //             break;
     //     }
     // }
 }
