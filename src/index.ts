a453cced9617354070dbe89f3f3c92ab6cc544be
genshun <genshun9@gmail.com>
2025-03-02 02:01:55 +0900

    全体の修正

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,9 @@ const app = express();
 const PORT:number = 3000;
 const ROOT:string = "/api"
 
-export default {
-    run() {
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
-        app.use(ROOT, router);
-        app.listen(PORT, () => {
-            process.stdout.write(`server started listening on port ${PORT}\n`);
-        });
-    }
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(ROOT, router);
+app.listen(PORT, () => {
+    process.stdout.write(`server started listening on port ${PORT}\n`);
+});
