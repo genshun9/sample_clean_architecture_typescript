fcad47b96b5aa4fc6b36d391fc63da7eb70b4db2
genshun <genshun9@gmail.com>
2025-03-03 02:03:31 +0900

    body-parser不要になったので削除

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import express from 'express'
-import * as bodyParser from 'body-parser'
 import router from './router'
 import {initializeApplication} from "./container";
 
@@ -11,8 +10,10 @@ const app = express();
 const PORT:number = 3000;
 const ROOT:string = "/api"
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// req.bodyを受け取るために設定。body-parserは現代では不要
+app.use(express.urlencoded({ extended: true }));
+// jsonを返却するために設定
+app.use(express.json());
 app.use(ROOT, router);
 app.listen(PORT, () => {
     process.stdout.write(`server started listening on port ${PORT}\n`);
